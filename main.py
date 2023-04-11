ffd931e16ed8edf99ed6308a552676613dab6de5
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-11 16:48:22 +1200

    Implemented scrape

--- main.py
+++ main.py
@@ -1,31 +1,155 @@
+import time
+import datetime
+from typing import Optional
 from OSRS_Hiscores import Hiscores
+from pydantic import BaseModel
 
-# User to lookup
-username = 'Shupwup'
 
-# Initialize user object, if no account type is specified, we assume 'N'
-user = Hiscores(username, 'N')
+class StatInfo(BaseModel):
+  rank: int
+  level: int
+  experience: int
+  next_level_exp: Optional[int]
+  exp_to_next_level: Optional[int]
+
+class BossInfo(BaseModel):
+  rank: int
+  kills: int
+
+
+class UserInfo(BaseModel):
+  # dict of skills <str, StatInfo>
+  # dict of bosses <str, BossInfo>
+  username: str
+  skills: dict[str, StatInfo]
+  bosses: dict[str, BossInfo]
+
+class OsrsScrape(BaseModel):
+  timestamp: float
+  users: list[UserInfo]
+
 
 # Get the entire stat dictionary
-user.stats
+_BOSSES = [
+  "Bounty Hunter - Hunter",
+  "Bounty Hunter - Rogue",
+  "Clue Scrolls (all)",
+  "Clue Scrolls (beginner)",
+  "Clue Scrolls (easy)",
+  "Clue Scrolls (medium)",
+  "Clue Scrolls (hard)",
+  "Clue Scrolls (elite)",
+  "Clue Scrolls (master)",
+  "LMS - Rank",
+  "PvP Arena - Rank",
+  "Soul Wars Zeal",
+  "Rifts closed",
+  "Abyssal Sire",
+  "Alchemical Hydra",
+  "Barrows Chests",
+  "Bryophyta",
+  "Callisto",
+  "Cerberus",
+  "Chambers of Xeric",
+  "Chambers of Xeric: Challenge Mode",
+  "Chaos Elemental",
+  "Chaos Fanatic",
+  "Commander Zilyana",
+  "Corporeal Beast",
+  "Crazy Archaeologist",
+  "Dagannoth Prime",
+  "Dagannoth Rex",
+  "Dagannoth Supreme",
+  "Deranged Archaeologist",
+  "General Graardor",
+  "Giant Mole",
+  "Grotesque Guardians",
+  "Hespori",
+  "Kalphite Queen",
+  "King Black Dragon",
+  "Kraken",
+  "Kree'Arra",
+  "K'ril Tsutsaroth",
+  "Mimic",
+  "Nex",
+  "Nightmare",
+  "Phosani's Nightmare",
+  "Obor",
+  "Phantom Muspah",
+  "Sarachnis",
+  "Scorpia",
+  "Skotizo",
+  "Tempoross",
+  "The Gauntlet",
+  "The Corrupted Gauntlet",
+  "Theatre of Blood",
+  "Theatre of Blood: Hard Mode",
+  "Thermonuclear Smoke Devil",
+  "Tombs of Amascut",
+  "Tombs of Amascut: Expert Mode",
+  "TzKal-Zuk",
+  "TzTok-Jad",
+  "Venenatis",
+  "Vet'ion",
+  "Vorkath",
+  "Wintertodt",
+  "Zalcano",
+  "Zulrah",
+]
 
-# Get total Levels
-user.skill('total')
 
-# Get a specific skill's ranking, level, and experience
-user.stats['runecrafting']
 
-# Get skill's level, ranking, and experience separately
-user.stats['runecrafting']['level']
-user.stats['runecrafting']['rank']
-user.stats['runecrafting']['experience']
 
-# NEW
-user.stats['runecrafting']['next_level_experience'] # Total Exp needed for next level
-user.stats['runecrafting']['exp_to_next_level'] # Exp remaining til next level
+def get_user_data(username: str) -> UserInfo:
+  # Initialize user object, if no account type is specified, we assume 'N'
+  user = Hiscores(username, 'N')
 
-# A simpler way to just get a skill's attributes
-print("Current level:", user.skill('attack', 'level'))
-print("Current rank:", user.skill('attack', 'rank'))
-print("Current exp:", user.skill('attack', 'experience'))
-print("Exp remaining:", user.skill('attack','exp_to_next_level'))
+  skills = {
+    skill_name: StatInfo(**skill_data)
+    for skill_name, skill_data in user.stats.items()
+  }
+
+  boss_data = user.data[-(len(_BOSSES)*2 + 1):]
+
+  bosses = {
+    boss_name: BossInfo(
+      rank=int(boss_data[i*2]),
+      kills=int(boss_data[i*2+1])
+    )
+    for i, boss_name in enumerate(_BOSSES)
+    if boss_data[i*2] != "-1"
+  }
+
+  return UserInfo(
+    username=username,
+    skills=skills,
+    bosses=bosses
+  )
+
+
+def scrape_users(usernames: list[str]) -> OsrsScrape:
+  return OsrsScrape(
+    timestamp=time.time(),
+    users=[get_user_data(username) for username in usernames]
+  )
+
+
+def main():
+  users = [
+    "shupwup",
+    "jerome-o",
+    "telemascope",
+    "furion",
+  ]
+
+  data = scrape_users(users)
+
+  # save to /data/osrs/{file friendly date, hours, minutes}.json, with permissions 644
+  with open(f"/data/osrs/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}.json", "w") as f:
+    f.write(data.json(indent=4))
+
+
+if __name__ == "__main__":
+  import logging
+  logging.basicConfig(level=logging.INFO)
+  main()
